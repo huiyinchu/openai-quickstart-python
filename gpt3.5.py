2c7dec565a9ae0554be5db55188dba7b8ab8bb8e
Yunjing <huiyinchu@gmail.com>
2023-03-02 22:38:39 +0800

    try gpt-3.5-turbo

---
+++ gpt3.5.py
@@ -0,0 +1,21 @@
+# Note: you need to be using OpenAI Python v0.27.0 for the code below to work
+import openai
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "Who won the world series in 2020?"},
+        {"role": "assistant",
+            "content": "The Los Angeles Dodgers won the World Series in 2020."},
+        {"role": "user", "content": "Where was it played?"}
+    ]
+)
+
+print(response)
