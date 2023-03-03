a88f4294029de9e4283cb80d571dd49e0d4b645c
xi <xi@yunjinginc.com>
2023-03-03 09:52:26 +0800

    user can input question

--- gpt3.5.py
+++ gpt3.5.py
@@ -5,17 +5,17 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+messages = [{"role": "system", "content": "You are a helpful assistant."}, ]
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-response = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": "Who won the world series in 2020?"},
-        {"role": "assistant",
-            "content": "The Los Angeles Dodgers won the World Series in 2020."},
-        {"role": "user", "content": "Where was it played?"}
-    ]
-)
-
-print(response)
+while True:
+    message = input("Yinchu: ")
+    if message:
+        messages.append({"role": "user", "content": message},)
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages
+    )
+    reply = response.choices[0].message.content
+    print(f"ChatGPT: {reply}\n")
+    messages.append({"role": "assistant", "content": reply})
